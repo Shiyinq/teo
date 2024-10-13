e3385b92db901cbee709076d568cafe95278c085
Shiyinq <shiyinq.github.io@gmail.com>
2024-10-13 08:59:44 +0700

    fix: chat id

--- internal/services/bot/service/service.go
+++ internal/services/bot/service/service.go
@@ -77,7 +77,7 @@ func (r *BotServiceImpl) Bot(chat *model.TelegramIncommingChat) (*model.Telegram
 		response = conv.Content
 	}
 
-	send, err := sendTelegramMessage(chat.Message.From.Id, chat.Message.MessageId, response)
+	send, err := sendTelegramMessage(chat.Message.Chat.Id, chat.Message.MessageId, response)
 	if err != nil || !send.Ok {
 		return nil, err
 	}
