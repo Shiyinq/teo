99af345979da8f0cd2c92a161c7ef5e7757eb498
Shiyinq <shiyinq.github.io@gmail.com>
2024-10-16 11:53:11 +0700

    feat: ney type user message struct

--- internal/services/bot/model/model.go
+++ internal/services/bot/model/model.go
@@ -21,11 +21,49 @@ type User struct {
 
 // telegram format message
 type UserMessage struct {
-	Chat      Chat   `json:"chat"`
-	Date      int64  `json:"date"`
-	From      From   `json:"from"`
-	MessageId int    `json:"message_id"`
-	Text      string `json:"text"`
+	Chat      Chat      `json:"chat"`
+	Date      int64     `json:"date"`
+	From      From      `json:"from"`
+	MessageId int       `json:"message_id"`
+	Text      string    `json:"text,omitempty"`
+	Photo     []Photo   `json:"photo,omitempty"`
+	Document  *Document `json:"document,omitempty"`
+	Caption   string    `json:"caption,omitempty"`
+	Voice     *Voice    `json:"voice,omitempty"`
+}
+
+type Photo struct {
+	FileID       string `json:"file_id"`
+	FileUniqueID string `json:"file_unique_id"`
+	FileSize     int    `json:"file_size"`
+	Width        int    `json:"width"`
+	Height       int    `json:"height"`
+}
+
+type Thumbnail struct {
+	FileID       string `json:"file_id"`
+	FileUniqueID string `json:"file_unique_id"`
+	FileSize     int    `json:"file_size"`
+	Width        int    `json:"width"`
+	Height       int    `json:"height"`
+}
+
+type Document struct {
+	FileName     string    `json:"file_name"`
+	MimeType     string    `json:"mime_type"`
+	Thumbnail    Thumbnail `json:"thumbnail"`
+	Thumb        Thumbnail `json:"thumb"`
+	FileID       string    `json:"file_id"`
+	FileUniqueID string    `json:"file_unique_id"`
+	FileSize     int       `json:"file_size"`
+}
+
+type Voice struct {
+	Duration     int    `json:"duration"`
+	MimeType     string `json:"mime_type"`
+	FileID       string `json:"file_id"`
+	FileUniqueID string `json:"file_unique_id"`
+	FileSize     int    `json:"file_size"`
 }
 
 type Chat struct {
